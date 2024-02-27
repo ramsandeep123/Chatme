342c220752e8f96a568e524fa6d1691d61a54ad9
ram <ramsandeep00000@gmail.com>
2024-02-27 16:38:03 +0530

    second change after

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chatme-qafa.onrender.com/"],
+		origin: ["https://chatme-qafa.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -15,11 +15,14 @@ function useLogin() {
 
 		setLoading(true);
 		try {
-			const res = await fetch("http://localhost:5000/api/auth/login", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ username, password }),
-			});
+			const res = await fetch(
+				"https://chatme-qafa.onrender.com/api/auth/login",
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ username, password }),
+				}
+			);
 
 			const data = await res.json();
 
