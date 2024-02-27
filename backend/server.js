68c19bb1e2aa590af2045f18a679f40e3c8a53f1
ram <ramsandeep00000@gmail.com>
2024-02-27 15:15:48 +0530

    change cros

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:5173", // Replace with your frontend URL
+		origin: "http://localhost:5000", // Replace with your frontend URL
 		credentials: true,
 	})
 );
