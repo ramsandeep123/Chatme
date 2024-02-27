bb795aa53c41ec7e04fb6c1e4061aae0578308cf
ram <ramsandeep00000@gmail.com>
2024-02-27 16:04:45 +0530

    new change

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chatme-qafa.onrender.com/"],
+		origin: ["http://localhost:5173"],
 		methods: ["GET", "POST"],
 	},
 });
